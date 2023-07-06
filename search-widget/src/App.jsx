f768cce61a3f0bd1cae3ae9202f94b315f0b65b7
Mark Carson <markcarson121@gmail.com>
2023-07-06 10:24:06 +0200

    added active state variable

--- search-widget/src/App.jsx
+++ search-widget/src/App.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 import "./App.css";
 
+import search from "./icons/search.svg";
+
 export function App() {
-  return <>App</>;
+  const [active, isActive] = useState(false);
+
+  return (
+    <div className="search">
+      <input className="input" type="text" placeholder="Search" />
+      <button className="btn">
+        <img src={search} alt="Search" />
+      </button>
+    </div>
+  );
 }
