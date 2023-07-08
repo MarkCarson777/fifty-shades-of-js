f7b32dd018cb3bf8be61bfe98e91ff9721eae757
Mark Carson <markcarson121@gmail.com>
2023-07-08 13:35:55 +0200

    finished solar system

--- orbit/src/App.jsx
+++ orbit/src/App.jsx
@@ -1,5 +1,18 @@
-import "./App.css";
+import styles from "./App.module.css";
 
 export function App() {
-  return <>Orbit</>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.sun}></div>
+      <div className={styles.innerOrbit}>
+        <div className={styles.innerPlanet}></div>
+      </div>
+      <div className={styles.middleOrbit}>
+        <div className={styles.middlePlanet}></div>
+      </div>
+      <div className={styles.outerOrbit}>
+        <div className={styles.outerPlanet}></div>
+      </div>
+    </div>
+  );
 }
