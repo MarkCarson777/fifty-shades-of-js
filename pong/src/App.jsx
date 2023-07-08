03e450ef652c5a8511a04c235b76b93970cee03f
Mark Carson <markcarson121@gmail.com>
2023-07-08 10:11:27 +0200

    added simple setup for pong game

--- pong/src/App.jsx
+++ pong/src/App.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
+
 import "./App.css";
 
 export function App() {
-  return <>Pong</>;
+  const [paddle1Y, setPaddle1Y] = useState(0);
+  const [paddle2Y, setPaddle2Y] = useState(0);
+  const [ballX, setBallX] = useState(0);
+  const [ballY, setBallY] = useState(0);
+
+  useEffect(() => {
+    const onKeyDown = (event) => {
+      // update paddle positions depending on key presses
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    // move ball
+    // check for collisions
+  }, [paddle1Y, paddle2Y, ballX, ballY]);
+
+  return <div>{/*Render game components*/}</div>;
 }
