2050994696869601acff38ffc98fe5721fe6e9f4
Mark Carson <markcarson121@gmail.com>
2023-07-10 12:05:43 +0200

    changed CSS and labels

--- hover/src/App.jsx
+++ hover/src/App.jsx
@@ -1,3 +1,5 @@
+import classnames from "classnames";
+
 import "./App.css";
 
 import jungleOne from "./images/jungle-one.jpg";
@@ -6,19 +8,65 @@ import jungleThree from "./images/jungle-three.jpg";
 import jungleFour from "./images/jungle-four.jpg";
 
 const images = [
-  { id: 1, src: jungleOne, alt: "jungle", label: "This is an image" },
-  { id: 2, src: jungleTwo, alt: "jungle", label: "This is an image" },
-  { id: 3, src: jungleThree, alt: "jungle", label: "This is an image" },
-  { id: 4, src: jungleFour, alt: "jungle", label: "This is an image" },
+  {
+    id: 1,
+    src: jungleOne,
+    alt: "jungle",
+    label: "Blur",
+    styles: {
+      blur: true,
+      fade: true,
+    },
+  },
+  {
+    id: 2,
+    src: jungleTwo,
+    alt: "jungle",
+    label: "Zoom",
+    styles: {
+      fade: true,
+      zoom: true,
+    },
+  },
+  {
+    id: 3,
+    src: jungleThree,
+    alt: "jungle",
+    label: "Slide",
+    styles: { blur: true, slideLeft: true },
+  },
+  {
+    id: 4,
+    src: jungleFour,
+    alt: "jungle",
+    label: "Grayscale",
+    styles: { grayScale: true, slideLeft: true },
+  },
 ];
 
 export function App() {
   return (
     <div className="grid">
-      {images.map(({ id, src, alt, label }) => (
+      {images.map(({ id, src, alt, label, styles }) => (
         <div key={id} className="img-container">
-          <img src={src} alt={alt} />
-          <div className="label">{label}</div>
+          <img
+            src={src}
+            alt={alt}
+            className={classnames(
+              styles?.blur && "blur",
+              styles?.zoom && "zoom",
+              styles?.grayScale && "gray-scale"
+            )}
+          />
+          <div
+            className={classnames(
+              styles?.fade && "fade",
+              styles?.slideLeft && "slide-left",
+              "label"
+            )}
+          >
+            {label}
+          </div>
         </div>
       ))}
     </div>
