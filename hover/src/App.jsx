74912729ab944a5a1cf56335d7ebfee9eb43f870
Mark Carson <markcarson121@gmail.com>
2023-07-10 11:19:47 +0200

    created an array of image objects

--- hover/src/App.jsx
+++ hover/src/App.jsx
@@ -1,5 +1,17 @@
 import "./App.css";
 
+import jungleOne from "./images/jungleOne.jpg";
+import jungleTwo from "./images/jungleTwo.jpg";
+import jungleThree from "./images/jungleThree.jpg";
+import jungleFour from "./images/jungleFour.jpg";
+
+const images = [
+  { id: 1, src: jungleOne, alt: "jungle" },
+  { id: 2, src: jungleTwo, alt: "jungle" },
+  { id: 3, src: jungleThree, alt: "jungle" },
+  { id: 4, src: jungleFour, alt: "jungle" },
+];
+
 export function App() {
   return (
     <div>
