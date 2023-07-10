040cd293ed0df6c3f056faa8f1b15313d4eb35b1
Mark Carson <markcarson121@gmail.com>
2023-07-10 11:24:18 +0200

    mapped over each image object and rendered in JSX

--- hover/src/App.jsx
+++ hover/src/App.jsx
@@ -1,36 +1,26 @@
 import "./App.css";
 
-import jungleOne from "./images/jungleOne.jpg";
-import jungleTwo from "./images/jungleTwo.jpg";
-import jungleThree from "./images/jungleThree.jpg";
-import jungleFour from "./images/jungleFour.jpg";
+import jungleOne from "./images/jungle-one.jpg";
+import jungleTwo from "./images/jungle-two.jpg";
+import jungleThree from "./images/jungle-three.jpg";
+import jungleFour from "./images/jungle-four.jpg";
 
 const images = [
-  { id: 1, src: jungleOne, alt: "jungle" },
-  { id: 2, src: jungleTwo, alt: "jungle" },
-  { id: 3, src: jungleThree, alt: "jungle" },
-  { id: 4, src: jungleFour, alt: "jungle" },
+  { id: 1, src: jungleOne, alt: "jungle", label: "This is an image" },
+  { id: 2, src: jungleTwo, alt: "jungle", label: "This is an image" },
+  { id: 3, src: jungleThree, alt: "jungle", label: "This is an image" },
+  { id: 4, src: jungleFour, alt: "jungle", label: "This is an image" },
 ];
 
 export function App() {
   return (
     <div>
-      <div>
-        <img src="#" alt="image" />
-        <div>This is an image</div>
-      </div>
-      <div>
-        <img src="#" alt="image" />
-        <div>This is an image</div>
-      </div>
-      <div>
-        <img src="#" alt="image" />
-        <div>This is an image</div>
-      </div>
-      <div>
-        <img src="#" alt="image" />
-        <div>This is an image</div>
-      </div>
+      {images.map(({ id, src, alt, label }) => (
+        <div key={id}>
+          <img src={src} alt={alt} />
+          <div>{label}</div>
+        </div>
+      ))}
     </div>
   );
 }
