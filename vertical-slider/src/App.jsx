89d96f2be43d77109ca763d0d8e0e7fe196fbb3f
Mark Carson <markcarson121@gmail.com>
2023-07-11 16:53:52 +0200

    fully functional but front end needs work

--- vertical-slider/src/App.jsx
+++ vertical-slider/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from "react";
+
 import "./App.css";
 
 import up from "./icons/circle-chevron-up.svg";
@@ -16,34 +18,96 @@ const images = [
 ];
 
 export function App() {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(-1);
+  const [sliderHeight, setSliderHeight] = useState(0);
+
+  const sliderContainerRef = useRef(null);
+  const slideLeftRef = useRef(null);
+  const slideRightRef = useRef(null);
+
+  useEffect(() => {
+    const slidesLength = slideRightRef.current.querySelectorAll("div").length;
+    slideLeftRef.current.style.top = `-${(slidesLength - 1) * 100}vh`;
+    setSliderHeight(sliderContainerRef.current.clientHeight);
+  }, []);
+
+  const changeSlide = (direction) => {
+    const sliderHeight = sliderContainerRef.current.clientHeight;
+
+    if (direction === "up") {
+      setActiveSlideIndex((prevIndex) => prevIndex + 1);
+    } else if (direction === "down") {
+      setActiveSlideIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  useEffect(() => {
+    const slidesLength = slideRightRef.current.querySelectorAll("div").length;
+
+    if (activeSlideIndex > slidesLength - 1) {
+      setActiveSlideIndex(0);
+    } else if (activeSlideIndex < 0) {
+      setActiveSlideIndex(slidesLength - 1);
+    }
+
+    slideRightRef.current.style.transform = `translateY(-${
+      activeSlideIndex * sliderHeight
+    }px)`;
+    slideLeftRef.current.style.transform = `translateY(${
+      activeSlideIndex * sliderHeight
+    }px)`;
+  }, [activeSlideIndex, sliderHeight]);
+
   return (
-    <div className="container">
-      <div className="left-slide">
+    <div className="container" ref={sliderContainerRef}>
+      <div className="left-slide" ref={slideLeftRef}>
         <div
-          style={{ backgroundColor: "rgba(247, 115, 39, 1.0)", color: "white" }}
+          id={1}
+          style={{
+            backgroundColor: "rgba(60, 108, 80, 1.00)",
+            color: "white",
+          }}
         >
           <span>Waterfall One</span>
         </div>
-        <div>
+        <div
+          id={2}
+          style={{
+            backgroundColor: "rgba(196, 199, 199, 1.00)",
+            color: "white",
+          }}
+        >
           <span>Waterfall Two</span>
         </div>
-        <div>
+        <div
+          id={3}
+          style={{
+            backgroundColor: "rgba(76, 124, 159, 1.00)",
+            color: "white",
+          }}
+        >
           <span>Waterfall Three</span>
         </div>
-        <div>
+        <div
+          id={4}
+          style={{
+            backgroundColor: "rgba(234, 140, 84, 1.00)",
+            color: "white",
+          }}
+        >
           <span>Waterfall Four</span>
         </div>
       </div>
-      <div className="right-slide">
+      <div className="right-slide" ref={slideRightRef}>
         {images.map(({ id, image }) => (
           <div key={id} style={{ backgroundImage: `url(${image})` }} />
         ))}
       </div>
       <div className="buttons">
-        <button className="up-button">
+        <button className="up-button" onClick={() => changeSlide("up")}>
           <img src={up} />
         </button>
-        <button className="down-button">
+        <button className="down-button" onClick={() => changeSlide("down")}>
           <img src={down} />
         </button>
       </div>
