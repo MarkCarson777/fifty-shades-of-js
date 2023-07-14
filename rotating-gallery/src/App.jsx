f3b56cf345c05b396420b6b3c9548e3455819325
Mark Carson <markcarson121@gmail.com>
2023-07-14 12:05:48 +0200

    imported images

--- rotating-gallery/src/App.jsx
+++ rotating-gallery/src/App.jsx
@@ -1,5 +1,25 @@
 import "./App.css";
 
+import snowOne from "./images/snow-one.jpg";
+import snowTwo from "./images/snow-two.jpg";
+import snowThree from "./images/snow-three.jpg";
+import snowFour from "./images/snow-four.jpg";
+import snowFive from "./images/snow-five.jpg";
+import snowSix from "./images/snow-six.jpg";
+import snowSeven from "./images/snow-seven.jpg";
+import snowEight from "./images/snow-eight.jpg";
+
+const images = [
+  { id: 1, src: snowOne, alt: "Snow image" },
+  { id: 2, src: snowTwo, alt: "Snow image" },
+  { id: 3, src: snowThree, alt: "Snow image" },
+  { id: 4, src: snowFour, alt: "Snow image" },
+  { id: 5, src: snowFive, alt: "Snow image" },
+  { id: 6, src: snowSix, alt: "Snow image" },
+  { id: 7, src: snowSeven, alt: "Snow image" },
+  { id: 8, src: snowEight, alt: "Snow image" },
+];
+
 export function App() {
   return <>Rotating Gallery</>;
 }
