ee7145cbfe573291d2c30df1d4dcdfcadf3ecb29
Mark Carson <markcarson121@gmail.com>
2023-07-20 13:20:32 +0200

    received videos from api

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,5 +1,10 @@
+import useVideos from "./hooks/useVideos";
+
 import "./App.css";
 
 export function App() {
+  const [videos, search] = useVideos("sleep token");
+  console.log("videos", videos);
+  console.log("search", search);
   return <>Youtube</>;
 }
