925004d714efca946624aea9c52389310a38b118
Mark Carson <markcarson121@gmail.com>
2023-07-20 14:46:35 +0200

    added search functionality

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 import { useVideos } from "./hooks/useVideos";
 
 import "./App.css";
 
 export function App() {
+  const [term, setTerm] = useState("");
   const [videos, search] = useVideos("sleep token");
   console.log("videos", videos);
   console.log("search", search);
@@ -11,6 +14,19 @@ export function App() {
 
   return (
     <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          search(term);
+        }}
+      >
+        <input
+          placeholder="Search"
+          type="text"
+          value={term}
+          onChange={(event) => setTerm(event.target.value)}
+        />
+      </form>
       <iframe title="video player" src={videoSrc} />
       <span>{videos[0]?.snippet.title}</span>
     </>
