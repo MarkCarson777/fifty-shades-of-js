08f0e83d634cf946dc76f390c25c0a61b8e9f84b
Mark Carson <markcarson121@gmail.com>
2023-07-21 13:47:54 +0200

    created function to display how many hours ago video was published

--- youtube-clone/src/components/Video/index.jsx
+++ youtube-clone/src/components/Video/index.jsx
@@ -1,6 +1,22 @@
 import "./index.css";
 
 export function Video({ video }) {
+  function timeElapsedString(date) {
+    const now = new Date();
+    const timeDifferenceInMillis = now - date;
+
+    const millisInHour = 60 * 60 * 1000;
+    const hoursElapsed = Math.floor(timeDifferenceInMillis / millisInHour);
+
+    if (hoursElapsed === 1) {
+      return "1 hour ago";
+    } else if (hoursElapsed > 1 && hoursElapsed <= 24) {
+      return hoursElapsed + " hours ago";
+    } else {
+      return "More than 24 hours ago";
+    }
+  }
+
   return (
     <div className="video-container">
       <img
@@ -12,7 +28,9 @@ export function Video({ video }) {
         <p className="video-title">{video.snippet.title}</p>
         <p className="video-channel">{video.snippet.channelTitle}</p>
         <p className="video-published">
-          {new Date(video.snippet.publishedAt).toLocaleString()}
+          {timeElapsedString(
+            new Date(video.snippet.publishedAt).toLocaleString()
+          )}
         </p>
       </div>
     </div>
