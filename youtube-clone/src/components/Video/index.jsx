c0c09d99213df3e8bcd71106396689ce7ba60cdc
Mark Carson <markcarson121@gmail.com>
2023-07-21 13:59:20 +0200

    added condition to handle days

--- youtube-clone/src/components/Video/index.jsx
+++ youtube-clone/src/components/Video/index.jsx
@@ -2,18 +2,22 @@ import "./index.css";
 
 export function Video({ video }) {
   function timeElapsedString(date) {
+    console.log("date", date);
     const now = new Date();
     const timeDifferenceInMillis = now - date;
 
     const millisInHour = 60 * 60 * 1000;
     const hoursElapsed = Math.floor(timeDifferenceInMillis / millisInHour);
-
+    console.log("hours", hoursElapsed);
     if (hoursElapsed === 1) {
       return "1 hour ago";
     } else if (hoursElapsed > 1 && hoursElapsed <= 24) {
       return hoursElapsed + " hours ago";
+    } else if (hoursElapsed > 24 && hoursElapsed <= 48) {
+      return "1 day ago";
     } else {
-      return "More than 24 hours ago";
+      const daysElapsed = Math.floor(hoursElapsed / 24);
+      return daysElapsed + " days ago";
     }
   }
 
@@ -28,9 +32,7 @@ export function Video({ video }) {
         <p className="video-title">{video.snippet.title}</p>
         <p className="video-channel">{video.snippet.channelTitle}</p>
         <p className="video-published">
-          {timeElapsedString(
-            new Date(video.snippet.publishedAt).toLocaleString()
-          )}
+          {timeElapsedString(new Date(video.snippet.publishedAt))}
         </p>
       </div>
     </div>
