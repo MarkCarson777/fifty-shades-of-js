e05641ae03c374d0d1c4a0249a05459c2d5967c5
Mark Carson <markcarson121@gmail.com>
2023-07-21 14:29:09 +0200

    increased number of videos retrieved from api and adjusted thumbnail image

--- youtube-clone/src/components/VideoList/index.jsx
+++ youtube-clone/src/components/VideoList/index.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 export function VideoList({ videos }) {
   return (
     <div className="video-list">
-      {videos.map((video, index) => {
+      {videos.slice(1).map((video, index) => {
         return <Video key={index} video={video} />;
       })}
     </div>
