8583d3a2c8fc074e4e8e5e80761e37d8bafeb288
Mark Carson <markcarson121@gmail.com>
2023-07-21 13:09:54 +0200

    created Video component and mapped the VideoList

---
+++ youtube-clone/src/components/Video/index.jsx
@@ -0,0 +1,4 @@
+export function Video({ video }) {
+  console.log("video", video);
+  return <div>Video</div>;
+}
--- youtube-clone/src/components/VideoList/index.jsx
+++ youtube-clone/src/components/VideoList/index.jsx
@@ -1,3 +1,11 @@
-export function VideoList() {
-  return <div>Video List</div>;
+import { Video } from "../Video";
+
+export function VideoList({ videos }) {
+  return (
+    <div>
+      {videos.map((video) => {
+        return <Video video={video} />;
+      })}
+    </div>
+  );
 }
