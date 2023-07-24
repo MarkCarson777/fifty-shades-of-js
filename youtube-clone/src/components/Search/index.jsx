1949504c5a7b12f925527e4a3daf6f194a3c0810
Mark Carson <markcarson121@gmail.com>
2023-07-24 12:25:24 +0200

    added CSS to Search component

--- youtube-clone/src/components/Search/index.jsx
+++ youtube-clone/src/components/Search/index.jsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
 
+import "./index.css";
+
 export function Search({ search }) {
   const [term, setTerm] = useState("");
 
   return (
     <form
+      className="search-container"
       onSubmit={(event) => {
         event.preventDefault();
         search(term);
       }}
     >
       <input
+        className="search-input"
         placeholder="Search"
         type="text"
         value={term}
         onChange={(event) => setTerm(event.target.value)}
       />
-      <button>Search</button>
+      <button className="search-btn">Search</button>
     </form>
   );
 }
