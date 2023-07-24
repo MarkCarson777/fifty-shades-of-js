8c09058d48c2c0f6e76865cda216a51d77ab5272
Mark Carson <markcarson121@gmail.com>
2023-07-24 13:33:23 +0200

    resived VideoPlayer component

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -2,22 +2,18 @@ import { useVideos } from "./hooks/useVideos";
 
 import { Search } from "./components/Search";
 import { VideoList } from "./components/VideoList";
+import { VideoPlayer } from "./components/VideoPlayer";
 
 import "./App.css";
 
 export function App() {
   const [videos, search] = useVideos("sleep token");
 
-  const videoSrc = `https://www.youtube.com/embed/${videos[0]?.id.videoId}`;
-
   return (
     <div className="page-container">
       <Search search={search} />
       <div className="flex">
-        <div>
-          <iframe title="video player" src={videoSrc} />
-          <p>{videos[0]?.snippet.title}</p>
-        </div>
+        <VideoPlayer videos={videos} />
         <VideoList videos={videos} />
       </div>
     </div>
--- youtube-clone/src/components/VideoPlayer/index.jsx
+++ youtube-clone/src/components/VideoPlayer/index.jsx
@@ -1,3 +1,12 @@
-export function VideoPlayer() {
-  return <div>Video Player</div>;
+import "./index.css";
+
+export function VideoPlayer({ videos }) {
+  const videoSrc = `https://www.youtube.com/embed/${videos[0]?.id.videoId}`;
+
+  return (
+    <div className="video-player-container">
+      <iframe className="video-player" title="video player" src={videoSrc} />
+      <p>{videos[0]?.snippet.title}</p>
+    </div>
+  );
 }
