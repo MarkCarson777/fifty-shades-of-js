b88e8f663f16d42f13f203f3e376688bfb06d7c4
Mark Carson <markcarson121@gmail.com>
2023-07-25 18:14:47 +0200

    added buttons to VideoToolbar

--- youtube-clone/src/components/VideoToolbar/index.jsx
+++ youtube-clone/src/components/VideoToolbar/index.jsx
@@ -1,5 +1,14 @@
 import "./index.css";
 
 export function VideoToolbar() {
-  return <div className="video-toolbar-container">Toolbar</div>;
+  return (
+    <div className="video-toolbar-container">
+      <button>Subscribe</button>
+      <button>Like</button>
+      <button>Dislike</button>
+      <button>Share</button>
+      <button>Download</button>
+      <button>More</button>
+    </div>
+  );
 }
