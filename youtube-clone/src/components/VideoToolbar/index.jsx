d5dfd6780229d690405214e50d4ee69ee3bef774
Mark Carson <markcarson121@gmail.com>
2023-07-28 12:00:49 +0200

    added icos to VideoToolbar

--- youtube-clone/src/components/VideoToolbar/index.jsx
+++ youtube-clone/src/components/VideoToolbar/index.jsx
@@ -1,14 +1,28 @@
+import { Icon } from "../Icon";
+
 import "./index.css";
 
 export function VideoToolbar() {
   return (
     <div className="video-toolbar-container">
       <button className="video-toolbar-subscribe">Subscribe</button>
-      <button className="video-toolbar-like">Like</button>
-      <button className="video-toolbar-dislike">Dislike</button>
-      <button className="video-toolbar-share">Share</button>
-      <button className="video-toolbar-download">Download</button>
-      <button className="video-toolbar-menu">Menu</button>
+      <button className="video-toolbar-like">
+        <Icon icon="Like" />
+      </button>
+      <button className="video-toolbar-dislike">
+        <Icon icon="Dislike" />
+      </button>
+      <button className="video-toolbar-share">
+        <Icon icon="Share" />
+        Share
+      </button>
+      <button className="video-toolbar-download">
+        <Icon icon="Download" />
+        Download
+      </button>
+      <button className="video-toolbar-menu">
+        <Icon icon="VerticalEllipsis" />
+      </button>
     </div>
   );
 }
