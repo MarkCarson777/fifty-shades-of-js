979a2c08f954969c0817bb0fee0e60d2545724a0
Mark Carson <markcarson121@gmail.com>
2023-07-28 16:31:00 +0200

    added icons to Header and started positioning

--- youtube-clone/src/components/Header/index.jsx
+++ youtube-clone/src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 
+import { Icon } from "../Icon";
 import { Search } from "../Search";
 
 import "./index.css";
@@ -7,7 +8,23 @@ import "./index.css";
 export function Header({ search }) {
   return (
     <div className="header-container">
+      <button>
+        <Icon icon="Menu" />
+      </button>
+
       <Search search={search} />
+      <button className="header-microphone">
+        <Icon icon="Microphone" />
+      </button>
+
+      <div>
+        <button>
+          <Icon icon="Video" />
+        </button>
+        <button>
+          <Icon icon="Bell" />
+        </button>
+      </div>
     </div>
   );
 }
