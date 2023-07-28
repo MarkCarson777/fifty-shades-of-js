4cbefaf278872f51c091fa22e188a0bcd039ee48
Mark Carson <markcarson121@gmail.com>
2023-07-28 12:22:53 +0200

    finished VideoToolbar styling

--- youtube-clone/src/components/VideoToolbar/index.jsx
+++ youtube-clone/src/components/VideoToolbar/index.jsx
@@ -6,23 +6,25 @@ export function VideoToolbar() {
   return (
     <div className="video-toolbar-container">
       <button className="video-toolbar-subscribe">Subscribe</button>
-      <button className="video-toolbar-like">
-        <Icon icon="Like" />
-      </button>
-      <button className="video-toolbar-dislike">
-        <Icon icon="Dislike" />
-      </button>
-      <button className="video-toolbar-share">
-        <Icon icon="Share" />
-        Share
-      </button>
-      <button className="video-toolbar-download">
-        <Icon icon="Download" />
-        Download
-      </button>
-      <button className="video-toolbar-menu">
-        <Icon icon="VerticalEllipsis" />
-      </button>
+      <div className="video-toolbar-container">
+        <button className="video-toolbar-like">
+          <Icon icon="Like" />
+        </button>
+        <button className="video-toolbar-dislike">
+          <Icon icon="Dislike" />
+        </button>
+        <button className="video-toolbar-share">
+          <Icon icon="Share" />
+          Share
+        </button>
+        <button className="video-toolbar-download">
+          <Icon icon="Download" />
+          Download
+        </button>
+        <button className="video-toolbar-menu">
+          <Icon icon="VerticalEllipsis" />
+        </button>
+      </div>
     </div>
   );
 }
