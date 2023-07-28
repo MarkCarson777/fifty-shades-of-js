b405847c487548f9b4de7e68b739f535ff2fb2b4
Mark Carson <markcarson121@gmail.com>
2023-07-28 11:54:51 +0200

    added icons to Icon component

--- youtube-clone/src/components/Icon/index.jsx
+++ youtube-clone/src/components/Icon/index.jsx
@@ -1,13 +1,31 @@
 import PropTypes from "prop-types";
 
+import { ReactComponent as Bell } from "../../icons/bell.svg";
 import { ReactComponent as Clock } from "../../icons/clock.svg";
+import { ReactComponent as Dislike } from "../../icons/thumbs-down.svg";
+import { ReactComponent as Download } from "../../icons/download.svg";
+import { ReactComponent as Like } from "../../icons/thumbs-up.svg";
+import { ReactComponent as Menu } from "../../icons/icon-menu.svg";
+import { ReactComponent as Microphone } from "../../icons/microphone.svg";
 import { ReactComponent as RectangleList } from "../../icons/rectangle-list.svg";
+import { ReactComponent as Search } from "../../icons/magnifying-glass.svg";
+import { ReactComponent as Share } from "../../icons/share-from-square.svg";
 import { ReactComponent as VerticalEllipsis } from "../../icons/ellipsis-vertical.svg";
+import { ReactComponent as Video } from "../../icons/video.svg";
 
 export const icons = {
+  Bell,
   Clock,
+  Dislike,
+  Download,
+  Like,
+  Menu,
+  Microphone,
   RectangleList,
+  Search,
+  Share,
   VerticalEllipsis,
+  Video,
 };
 
 export function Icon(props) {
