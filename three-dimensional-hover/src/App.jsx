826a1a05b5e47a5d9f3a6b999e02e6441c80f3df
Mark Carson <markcarson121@gmail.com>
2023-08-09 12:20:50 +0200

    finished three-dimensional-hover

--- three-dimensional-hover/src/App.jsx
+++ three-dimensional-hover/src/App.jsx
@@ -1,5 +1,16 @@
+import mountain from "./images/mountain.jpg";
+import overlay from "./images/mountain-overlay.png";
+
 import "./App.css";
 
 export function App() {
-  return <>App</>;
+  return (
+    <div className="card">
+      <div className="wrapper">
+        <img src={mountain} className="background-img" alt="Mountain" />
+      </div>
+      <h1 className="title">Yosemite</h1>
+      <img className="overlay" src={overlay} alt="Mountain peak" />
+    </div>
+  );
 }
